ce925286b9d062315af32296858df257206d9484
oussamelfig <oelfigha2002@gmail.com>
2023-03-29 10:45:56 -0400

    deploy my app

--- testEmail.py
+++ testEmail.py
@@ -8,7 +8,8 @@ with open("email_config.yaml", 'r') as stream:
 sample_new_contrevenants = [
     "Établissement 1",
     "Établissement 2",
-    "Établissement 3"
+    "Établissement 3",
+    "Établissement 4"
 ]
 
 send_new_contrevenants_email(sample_new_contrevenants, email_config)
