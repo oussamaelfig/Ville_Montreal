7e05af02f953529707c293e23c0571f28f4df3e3
oussamelfig <oelfigha2002@gmail.com>
2023-04-17 14:25:30 -0400

    add data db

--- effacer_db.py
+++ effacer_db.py
@@ -6,6 +6,8 @@ c = conn.cursor()
 
 # Clear all the data in the table
 c.execute('DELETE FROM poursuite')
+c.execute('DELETE FROM utilisateurs')
+c.execute('DELETE FROM plaintes')
 
 # Commit the changes and close the database connection
 conn.commit()
